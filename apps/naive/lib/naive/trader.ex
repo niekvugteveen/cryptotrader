c6069cc4764150fcb3b36419ae5d8d2044a99c4f
niekvugteveen <info@niekvugteveen.nl>
2022-06-27 19:54:15 +0200

    Simpele trader

---
+++ apps/naive/lib/naive/trader.ex
@@ -0,0 +1,23 @@
+defmodule Naive.Trader do
+	use GenServer
+	require Logger
+
+	def start_link(args) do
+		GenServer.start_link(__MODULE__, args, name: :trader)
+	end
+
+	def init(args) do
+		{:ok, args}
+	end
+end
+
+defmodule State do
+	@enforce_keys [:symbol, :profit_interval, :tick_size]
+	defstruct [
+		:symbol,
+		:buy_order,
+		:sell_order,
+		:profit_interval,
+		:tick_size
+	]
+end

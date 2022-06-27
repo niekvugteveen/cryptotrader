f7f4f4ddccd672575aaea0c0e2dd41ee849389e5
niekvugteveen <info@niekvugteveen.nl>
2022-06-27 20:03:39 +0200

    Simpele trader aangevuld

--- apps/naive/lib/naive/trader.ex
+++ apps/naive/lib/naive/trader.ex
@@ -6,8 +6,29 @@ defmodule Naive.Trader do
 		GenServer.start_link(__MODULE__, args, name: :trader)
 	end
 
-	def init(args) do
-		{:ok, args}
+	def init(%{symbol: symbol, profit_interval: profit_interval}) do
+		symbol = String.upcase(symbol)
+
+		Logger.info("Initializing new trader for #{symbol}")
+
+		tick_size = fetch_tick_size(symbol)
+
+		{:ok,
+			%State{
+			symbol: symbol,
+			profit_interval: profit_interval,
+			tick_size: tick_size
+		}}
+	end
+
+	defp fetch_tick_size(symbol) do
+		Binance.get_exchange_info()
+		|> elem(1)
+		|> Map.get(:symbols)
+		|> Enum.find(&(&1["symbol"] == symbol))
+		|> Map.get("filters")
+		|> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
+		|> Map.get("tickSize")
 	end
 end
 
